5f5cffc8d15d82d92a20c794e02e2853b16d0a28
omthapa779 <omthapa781@gmail.com>
2025-03-22 23:14:12 +0545

    Hero Section Completed With Responsiveness
    Next Update:
    - About us Section
    - Home Page completion

--- resources/views/layouts/navs.blade.php
+++ resources/views/layouts/navs.blade.php
@@ -13,8 +13,8 @@
 </head>
 <body>
     <div class="main_container padding_vxs">
-        <navs class="navbar w_100 h_10vh padding_s10 flex justify_sb ">
-            <div class="right_nav h_100 flex align_c gap_xs">
+        <navs class="navbar w_100 h_10vh padding_s10 flex justify_sa ">
+            <div class="right_nav h_100 w_100 flex align_c gap_xs">
                 <img src="{{ asset('./resources/images/logo.png') }}" alt="Logo | Natural Agro and Herbs" class="h_100">
                 <div class="links_holder flex gap_xs">
                     <h3><a href="/">Home</a></h3>
@@ -24,14 +24,13 @@
                     <h3><a href="/">Contact</a></h3>
                 </div>
             </div>
-            <div class="left_nav h_100 flex align_c gap_xs">
-                <h3><a href="#" class="primary_link_button button_hover">View Products</a></h3>
-                <h3><a href="#" class="primary_link_button secondary_link_button button_hover">Inquire Now!</a></h3>
+            <div class="left_nav h_100 flex  align_c gap_xs">
+                <h3 class="primary_link_button no_wrap"><a href="#" class="background_cl">View Products</a></h3>
+                <h3 class="primary_link_button secondary_link_button no_wrap"><a href="#">Inquire Now!</a></h3>
             </div>
         </navs>
         @yield('content')
     </div>
-    <!-- Load Lenis with defer to improve page load performance -->
     <script src="https://unpkg.com/lenis@1.2.3/dist/lenis.min.js" defer></script> 
     <script src="{{ asset('./resources/scripts/logic.js') }}" defer></script>
 </body>
